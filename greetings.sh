56b592c32e8255351fe0ffee4f9970a63ba284dd
Dheison Gomes <dheisomgomes0@gmail.com>
2023-06-13 10:46:30 -0300

    Remove unused subfunction

--- greetings.sh
+++ greetings.sh
@@ -5,8 +5,8 @@ hour=$(date +%H)
 icon=gnome-robots
 
 greeting="Hello, Sr. $name!"
-[[ $hour -gt 5 ]] && greeting="Good morning, Sr. $name!"
+[[ $hour -gt 4 ]] && greeting="Good morning, Sr. $name!"
 [[ $hour -gt 11 ]] && greeting="Good afternoon, Sr. $name!"
 [[ $hour -gt 17 ]] && greeting="Good evening, Sr. $name!"
 
-notify-send -i "$icon" "`echo -e $greeting`"
+notify-send -i "$icon" "$greeting"
