58870fac96e392d7e18f3ca6810c2d6217170fff
Dheison Gomes <dheisomgomes0@gmail.com>
2023-09-13 07:44:30 -0300

    Added --save-changes option and "toggle" action to theme manager

--- theme.py
+++ theme.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
 # Utility to set XFCE theme
 
-from os import environ
+import argparse
+import json
+import subprocess
+from os import environ, path
 from time import localtime, tzset
-import subprocess, json, argparse
+
+NOTIFY_TITLE = "Theme Manager"
+DEFAULT_ICON = "style"
+CONFIG_PATH = path.join(environ.get("HOME", "/root"), ".config/themes.json")
+try:
+    config = json.load(open(CONFIG_PATH))
+except:
+    config = {"themes": {}, "enabled": ""}
 
 
-CONFIG_PATH = environ.get('HOME', '/root') + '/.config/themes.json'
-try: config = json.load(open(CONFIG_PATH))
-except: config = {'themes': {}, 'enabled': ''}
+class CommandFailed(Exception):
+    pass
 
 
-class CommandFailed(Exception): pass
+def notify(message: str, icon: str = DEFAULT_ICON):
+    print(f'[THEME] {message}')
+    subprocess.run(["notify-send", "-i", icon, NOTIFY_TITLE, message])
 
-def notify(title: str, message: str):
-    subprocess.run(['notify-send', '-i', 'style', title, message])
+
+def cmd_run(cmd: str) -> tuple[int, str]:
+    process = subprocess.run(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    code = process.returncode
+    stderr = process.stderr.decode().strip()
+    stdout = process.stdout.decode().strip()
+    return code, (stdout if code == 0 else stderr).strip()
 
 
 def get_properties(channel: str) -> list:
@@ -27,29 +45,25 @@ def get_properties(channel: str) -> list:
     :rtype: list
     :raises ValueError: if the specified channel is not a valid channel
     """
-    command = ['xfconf-query', '-c', channel, '-l']
-    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if process.returncode != 0:
+    code, output = cmd_run(f"xfconf-query -c '{channel}' -l")
+    if code != 0:
         raise ValueError(f"{channel} is not a valid channel")
-    output = process.stdout.decode().strip()
-    return output.split('\n')
+    return output.split("\n")
 
 
-def xfconf_get(channel: str, property_name: str) -> str:
+def xfconf_get(channel: str, property: str) -> str:
     """
     This function retrieves the value of a property from the given XFCE channel using the `xfconf-query` command.
-    
+
     :param channel: A string representing the XFCE channel to retrieve the property value from.
-    :param property_name: A string representing the name of the property to retrieve the value of.
+    :param property: A string representing the name of the property to retrieve the value of.
     :return: A string representing the value of the property.
     :raises: CommandFailed, if the `xfconf-query` command fails to execute.
     """
-    cmd = f'xfconf-query -c "{channel}" -p "{property_name}"'
-    xfconf = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    if xfconf.returncode != 0:
-        raise CommandFailed(f"xfconf-query command failed: {xfconf.stderr.decode()}")
-    output = xfconf.stdout.decode()
-    return output.strip()
+    code, output = cmd_run(f"xfconf-query -c '{channel}' -p '{property}'")
+    if code != 0:
+        raise CommandFailed(f"xfconf-query command failed: {output}")
+    return output
 
 
 def xfconf_set(channel: str, property_name: str, value: str):
@@ -64,10 +78,11 @@ def xfconf_set(channel: str, property_name: str, value: str):
     :type value: str
     :raises CommandFailed: If the `xfconf-query` command fails.
     """
-    command = f'xfconf-query -c "{channel}" -p "{property_name}" -s "{value}"'
-    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    if process.returncode != 0:
-        raise CommandFailed(f"xfconf-query command failed: {process.stderr.decode().strip()}")
+    code, output = cmd_run(
+        f"xfconf-query -c '{channel}' -p '{property_name}' -s '{value}'"
+    )
+    if code != 0:
+        raise CommandFailed(f"xfconf-query command failed: {output}")
 
 
 def set_theme(theme: dict):
@@ -83,85 +98,113 @@ def set_theme(theme: dict):
 
     :return: None
     """
-    xfconf_set('xsettings', '/Net/ThemeName', theme['gtk'])
-    xfconf_set('xfwm4', '/general/theme', theme.get('xfwm', theme.get('gtk')))
-    xfconf_set('xsettings', '/Net/IconThemeName', theme['icons'])
-    xfconf_set('xsettings', '/Gtk/CursorThemeName', theme['cursor'])
-    for w, wallpaper in theme['wallpapers'].items():
-        xfconf_set('xfce4-desktop', w, wallpaper)
+    xfconf_set("xsettings", "/Net/ThemeName", theme["gtk"])
+    xfconf_set("xfwm4", "/general/theme", theme.get("xfwm", theme.get("gtk")))
+    xfconf_set("xsettings", "/Net/IconThemeName", theme["icons"])
+    xfconf_set("xsettings", "/Gtk/CursorThemeName", theme["cursor"])
+    for workspace, wallpaper in theme["wallpapers"].items():
+        xfconf_set("xfce4-desktop", workspace, wallpaper)
 
 
 def get_wallpapers() -> dict[str, str]:
     """
     Returns a dictionary of wallpapers for each workspace.
-    
-    :return: A dictionary where each key is the workspace name and each value is the 
+
+    :return: A dictionary where each key is the workspace name and each value is the
              wallpaper path.
     :rtype: dict[str, str]
     """
-    properties = get_properties('xfce4-desktop')
-    workspaces = [p for p in properties if '/last-image' in p]
+    properties = get_properties("xfce4-desktop")
+    workspaces = [p for p in properties if "/last-image" in p]
     wallpapers = {}
     for w in workspaces:
-        wallpapers[w] = xfconf_get('xfce4-desktop', w)
+        wallpapers[w] = xfconf_get("xfce4-desktop", w)
     return wallpapers
 
 
-get_theme = lambda: {
-    'gtk': xfconf_get('xsettings', '/Net/ThemeName'),
-    'xfwm': xfconf_get('xfwm4', '/general/theme'),
-    'icons': xfconf_get('xsettings', '/Net/IconThemeName'),
-    'cursor': xfconf_get('xsettings', '/Gtk/CursorThemeName'),
-    'wallpapers': get_wallpapers()
-}
+def get_theme():
+    return {
+        "gtk": xfconf_get("xsettings", "/Net/ThemeName"),
+        "xfwm": xfconf_get("xfwm4", "/general/theme"),
+        "icons": xfconf_get("xsettings", "/Net/IconThemeName"),
+        "cursor": xfconf_get("xsettings", "/Gtk/CursorThemeName"),
+        "wallpapers": get_wallpapers(),
+    }
 
 
 def save(name: str):
     """
     Saves a theme with the given name to the configuration file.
-    
+
     :param name: A string representing the name of the theme.
     :type name: str
     """
-    config['themes'][name] = get_theme()
-    json.dump(config, open(CONFIG_PATH, 'w'), indent=2)
+    config["themes"][name] = get_theme()
+    config["enabled"] = name
+    json.dump(config, open(CONFIG_PATH, "w"), indent=2)
 
 
 def load(name: str):
     """
-    Loads the theme with the given name if it is not currently loaded. 
+    Loads the theme with the given name if it is not currently loaded.
 
     :param name: The name of the theme to load.
     :type name: str
     """
-    if config['enabled'] == name:
+    if config["enabled"] == name:
+        notify(f'Already on "{name}" theme :)')
         return
-    notify('Changing theme', f'Loading "{name}" theme...')
-    set_theme(config['themes'][name])
-    config['enabled'] = name
-    json.dump(config, open(CONFIG_PATH, 'w'), indent=2)
+    try:
+        theme = config["themes"][name]
+    except:
+        notify(
+            f'Theme "{name}" was not found in your config!',
+            "script-error",
+        )
+        return
+    notify(f'Loading "{name}" theme...')
+    set_theme(theme)
+    config["enabled"] = name
+    json.dump(config, open(CONFIG_PATH, "w"), indent=2)
 
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', choices=['load', 'save', 'auto'])
-    parser.add_argument('--name', required=False)
+    parser.add_argument("action", choices=["load", "save", "auto", "toggle"])
+    parser.add_argument("--name", required=False, help="Theme name")
+    parser.add_argument(
+        "--save-changes",
+        required=False,
+        type=bool,
+        const=True,
+        nargs='?',
+        help="Save theme modifications before changing",
+    )
     args = parser.parse_args()
-    
-    if args.action in ('load', 'save') and args.name is None:
-        print("Theme name is missing.")
+
+    if args.action in ("load", "save") and args.name is None:
+        notify("Theme name is missing.", "gnome-warning")
         return 1
+    elif args.save_changes:
+        tname = config["enabled"]
+        save(tname)
 
     match args.action:
-        case 'auto':
+        case "auto":
             tzset()
             hour = localtime().tm_hour
-            theme = 'light' if hour > 6 and hour < 19 else 'dark'
+            theme = "light" if hour > 6 and hour < 18 else "dark"
             load(theme)
-        case 'save': save(args.name)
-        case 'load': load(args.name)
-    
+        case "toggle":
+            theme = "light" if config["enabled"] == "dark" else "dark"
+            load(theme)
+        case "save":
+            save(args.name)
+        case "load":
+            load(args.name)
+
     return 0
 
 
-exit(main())
+if __name__ == "__main__":
+    exit(main())
