0a168c8bb28d412ae8f67c7c2bc2919e16369792
Dheison Gomes <dheisomgomes0@gmail.com>
2026-01-04 08:00:42 -0500

    Fix switch between dark and light themes and add possibility of installing a desktop entry to toggle theme

--- theme.sh
+++ theme.sh
@@ -4,7 +4,6 @@ CONFIG_PATH="$HOME/.config/themes"
 mkdir -p "$CONFIG_PATH" &>/dev/null
 
 ACTION="$1"
-NAME="$2"
 OLD_NAME=$(cat "$CONFIG_PATH/enabled" 2>/dev/null)
 
 GTK_THEME=
@@ -12,6 +11,8 @@ XFWM_THEME=
 ICON_THEME=
 CURSOR_THEME=
 WALLPAPER=
+IS_DARK=0
+MONITOR=$(xfconf-query -c "xfce4-desktop" -l | grep '\-0/workspace0/last-image')
 
 function notify() {
   echo "[THEME] $1"
@@ -23,24 +24,33 @@ function get_config () {
   XFWM_THEME=$(xfconf-query -c "xfwm4" -p "/general/theme")
   ICON_THEME=$(xfconf-query -c "xsettings" -p "/Net/IconThemeName")
   CURSOR_THEME=$(xfconf-query -c "xsettings" -p "/Gtk/CursorThemeName")
-  WALLPAPER=$(xfconf-query -c "xfce4-desktop" -p "/backdrop/screen0/monitor0/last-image")
+  WALLPAPER=$(xfconf-query -c "xfce4-desktop" -p "$MONITOR")
+  if [ "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'default'" ]; then
+    IS_DARK=0
+  else
+    IS_DARK=1
+  fi
 }
 
-function set_config () {
-  xfconf-query -c "xsettings" -p "/Net/ThemeName" -s "$GTK_THEME"
+function apply_config () {
   xfconf-query -c "xfwm4" -p "/general/theme" -s "$XFWM_THEME"
+  xfconf-query -c "xsettings" -p "/Net/ThemeName" -s "$GTK_THEME"
   xfconf-query -c "xsettings" -p "/Net/IconThemeName" -s "$ICON_THEME"
   xfconf-query -c "xsettings" -p "/Gtk/CursorThemeName" -s "$CURSOR_THEME"
-  xfconf-query -c "xfce4-desktop" -p "/backdrop/screen0/monitor0/last-image" -s "$WALLPAPER"
-  echo "$NAME" > "$CONFIG_PATH/enabled"
+  xfconf-query -c "xfce4-desktop" -p "$MONITOR" -s "$WALLPAPER"
+  if [ "$IS_DARK" = 1 ]; then
+    gsettings set org.gnome.desktop.interface color-scheme 'prefers-dark' &>/dev/null
+  else
+    gsettings set org.gnome.desktop.interface color-scheme 'default' &>/dev/null
+  fi
 }
 
 function load_config() {
-  local file="$CONFIG_PATH/$NAME.sh"
+  local file="$CONFIG_PATH/$1.sh"
   if [ -e "$file" ]; then
     source "$file"
   else
-    echo "Theme not found: $NAME"
+    echo "Theme not found: $1"
     return 2
   fi
 }
@@ -48,49 +58,74 @@ function load_config() {
 function toggle() {
   notify "Switching theme..."
   if [ "$OLD_NAME" = "light" ]; then
-    NAME="dark"
+    theme="dark"
   else
-    NAME="light"
+    theme="light"
   fi
-  load_config
-  set_config
+  load_config "$theme"
+  apply_config
+  echo "$theme" > "$CONFIG_PATH/enabled"
 }
 
 function load() {
-  [[ ! -n "$NAME" ]] && echo "You don't set theme name!" && exit 3
-  notify "Loading $NAME theme..."
-  load_config
-  set_config
+  [[ ! -n "$1" ]] && echo "You don't set theme name!" && exit 3
+  notify "Loading $1 theme..."
+  load_config "$1"
+  apply_config
+  echo "$1" > "$CONFIG_PATH/enabled"
 }
 
 function save() {
-  if [ ! -n "$NAME" ]; then
+  if [ ! -n "$1" ]; then
     echo "Theme name not defined!"
     exit 2
   fi
-  notify "Saving $NAME theme..."
+  notify "Saving $1 theme..."
   get_config
-  cat << EOF > "$CONFIG_PATH/$NAME.sh"
+  cat << EOF > "$CONFIG_PATH/$1.sh"
 GTK_THEME='$GTK_THEME'
 XFWM_THEME='$XFWM_THEME'
 ICON_THEME='$ICON_THEME'
 CURSOR_THEME='$CURSOR_THEME'
 WALLPAPER='$WALLPAPER'
+IS_DARK=$IS_DARK
 EOF
 }
 
-HELP="usage: $(basename $0) action [name]
+HELP="usage: $(basename $0) action
 
 Actions:
-  load - Loads a saved theme
-  save - Save actual state of XFCE as a theme
-  toggle - Switch between light and dark themes
-
-Optional parameters:
-  name - Name of theme to load/saved"
+  load <name>     - Loads a saved theme
+  save <name>     - Save actual state of XFCE as a theme
+  toggle          - Switch between light and dark themes
+  install-toggler - Install a menu item to switch between light and dark themes
+  remove-toggler  - Remove theme toggler menu item"
 
 case "$1" in 
-  load|save|toggle) eval "$1"; exit $? ;;
+  save) save "$2" "$3" ;;
+  load) load "$2" ;;
+  toggle) toggle ;;
+  install-toggler)
+  cp "$0" "$HOME/.local/theme.sh"
+  cat << EOF > "$HOME/.local/share/applications/theme-toggler.desktop"
+[Desktop Entry]
+Name=Theme Switch
+Comment=Switch between dark and light themes.
+Exec=bash $HOME/.local/theme.sh toggle
+Icon=style
+Terminal=false
+Type=Application
+Categories=Settings;Appearance;
+EOF
+  ;;
+  remove-toggler)
+    rm "$HOME/.local/share/applications/theme-toggler.desktop" "$HOME/.local/theme.sh" 
+    if [ $? = 0 ];then
+      echo "Done."
+    else
+      echo "Fail!"
+    fi
+    ;;
   help) echo "$HELP"; exit 0 ;;
   *) echo "Unknown action: $1" && exit 1 ;;
 esac
